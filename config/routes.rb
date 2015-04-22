9086a3bcb85e3e380c05b4b89b32031ae73c77b0
Artur Janka <arthur.janka@gmail.com>
2015-04-22 20:49:44 +0000

    build out navigation and welcome controller, set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/faq'
+
+  get 'welcome/pricing'
+
+  get 'welcome/features'
+  
+  get 'welcome/login'
+
   resources :invoices
 
-  root to: 'invoices#index'
+  root to: 'welcomes#index'
   
 end
