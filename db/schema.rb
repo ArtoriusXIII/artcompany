0e90c1bcddfdfed1b5478e2e1e616a3b39cc85af
Artur Janka <arthur.janka@gmail.com>
2015-04-23 21:58:14 +0000

    cleaning after wrong migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422161545) do
+ActiveRecord::Schema.define(version: 20150423184030) do
+
+
 
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
